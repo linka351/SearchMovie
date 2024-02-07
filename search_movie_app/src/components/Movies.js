390ffbb35627af2da5e759da38c91e6a849482a1
Linka351 <linka351@gmail.com>
2024-02-07 20:04:36 +0100

    add: movie-image-and-some-styles

--- search_movie_app/src/components/Movies.js
+++ search_movie_app/src/components/Movies.js
@@ -1,11 +1,54 @@
+import { useEffect, useState } from "react";
+import "../styles/Movies.scss";
+
+const apiKey = "c9de2f7b31706574fa92cef28829a225";
+const IMG_URL = "https://image.tmdb.org/t/p/w500/";
 
 function Movies() {
-	
+	const [data, setData] = useState([]);
+	const [getPage, setGetPage] = useState(1);
+	const [pages, setPages] = useState(null);
+
+	const takeToNextPage = () => {
+		setGetPage(prev => prev + 1);
+	};
+
+	const takeToPreviousPage = () => {
+		setGetPage(prev => prev - 1);
+	};
+
+	useEffect(() => {
+		fetch(
+			`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=${getPage}?`
+		)
+			.then(response => response.json())
+			.then(data => {
+				setPages(data.total_pages);
+				setData(data.results);
+			});
+	}, [getPage]);
+
 	return (
 		<>
-			
+			<div className='buttons-direction'>
+				<button onClick={takeToPreviousPage}>Prev</button>
+				<p>{`Page ${getPage}/${pages}`}</p>
+				<button onClick={takeToNextPage}>Next</button>
+			</div>
+			<div className='movie-page'>
+				{data.map(item => {
+					return (
+						<div className='image'>
+							<img src={`${IMG_URL}${item.backdrop_path}`} alt={item.title} />
+							<div className='movie-description'>
+								<p>{item.title}</p>
+							</div>
+						</div>
+					);
+				})}
+			</div>
 		</>
 	);
 }
 
-export default Movies
+export default Movies;
--- search_movie_app/src/components/Navbar.js
+++ search_movie_app/src/components/Navbar.js
@@ -7,24 +7,22 @@ import "../styles/Burger.scss";
 import LoginPanel from "./LoginPanel";
 
 function Navbar() {
-	
 	const [toggle, setToggle] = useState(false);
 	const toggleMenu = () => {
 		setToggle(!toggle);
 	};
 
 	console.log(localStorage.name);
-	
 
 	return (
-		<>	
+		<>
 			<div className='navbar'>
 				<div className={"left-side-navbar"}>
 					{!toggle ? (
 						<FaBars className='fabar' onClick={toggleMenu} />
-						) : (
-							<FaXmark className='faxmark' onClick={toggleMenu} />
-							)}
+					) : (
+						<FaXmark className='faxmark' onClick={toggleMenu} />
+					)}
 
 					<Link className='navbar-link' to={"/movies"}>
 						Movies
@@ -38,17 +36,16 @@ function Navbar() {
 				</Link>
 				<div className='right-side-navbar'>
 					<Link className='navbar-link' to={"/search"}>
-						<FaMagnifyingGlass className="search" />
+						<FaMagnifyingGlass className='search' />
 					</Link>
 					<Link className='navbar-link' to={"/profile"}>
-							{localStorage.length === 1 ? JSON.parse(localStorage.name) : "Anon"}
+						{localStorage.length === 1 ? JSON.parse(localStorage.name) : "Anon"}
 					</Link>
 				</div>
 			</div>
 			{toggle && <Burger showMenu={toggle} />}
-							{localStorage.length === 0 && <LoginPanel />}
+			{localStorage.length === 0 && <LoginPanel />}
 			<Outlet />
-			
 		</>
 	);
 }
