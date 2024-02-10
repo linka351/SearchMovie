77bd75fc9773cdef8e36903838bbb76917663380
Linka351 <linka351@gmail.com>
2024-02-10 11:54:43 +0100

    add: apiToSearchPage

--- search_movie_app/src/components/Search.js
+++ search_movie_app/src/components/Search.js
@@ -1,24 +1,30 @@
 import { useEffect, useState } from "react";
 
-//https://api.themoviedb.org/3/search/keyword
 const apiKey = "c9de2f7b31706574fa92cef28829a225";
-//const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+const IMG_URL = "https://image.tmdb.org/t/p/w500/";
 
 function Search() {
 	const [value, setValue] = useState("");
 	const [movie, setMovie] = useState([]);
 
-	useEffect(() => {
-		fetch(`https://api.themoviedb.org/3/search/keyword?api_key=${apiKey}`)
-			.then(response => response.json())
-			.then(data => {
-				setMovie(data);
-			});
-	});
-
 	const search = e => {
 		setValue(e.target.value);
 	};
+
+	useEffect(() => {
+		fetch(
+			`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&include_adult=false&language=en-US&page=1&query=${value}'`
+		)
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
+				setMovie(data.results);
+			});
+	}, [value]);
+
+	// const onChange = (e) => {
+	// 	e.preventDefault()
+	// }
 
 	return (
 		<>
@@ -26,9 +32,14 @@ function Search() {
 				<input
 					type='text'
 					value={value}
-					onInput={search}
+					onChange={search}
 					placeholder='Czego Szukasz?'
 				/>
+				{movie.map(item => {
+					return (
+						<img src={`${IMG_URL}${item.backdrop_path}`} alt={item.title} />
+					);
+				})}
 			</div>
 		</>
 	);
