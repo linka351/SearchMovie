3b9d44886e2b4a44437cbe20bf5ee8881dd36ac2
Linka351 <linka351@gmail.com>
2024-02-13 18:29:12 +0100

    finish: moviePage

--- search_movie_app/src/components/Movies.js
+++ search_movie_app/src/components/Movies.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import "../styles/Movies.scss";
-//dodanie rzeczy do nawigacji
 
 const apiKey = "c9de2f7b31706574fa92cef28829a225";
 const IMG_URL = "https://image.tmdb.org/t/p/w500/";
