33cfa2fb0f3d0d77871c3ca2d9102b3ff2c29260
Linka351 <linka351@gmail.com>
2024-02-15 16:46:01 +0100

    add: MovieDetail

--- search_movie_app/src/components/Details.js
+++ search_movie_app/src/components/Details.js
@@ -1,15 +1,27 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+const apiKey = "c9de2f7b31706574fa92cef28829a225";
+const IMG_URL = "https://image.tmdb.org/t/p/w500/";
+//`https://api.themoviedb.org/3/movie/${params}?api_key=${apiKey}&language=en-US`
+
 function Details() {
 	const params = useParams();
-	const [singleElement, setSingleElement] = useState([]);
+	const [singleElement, setSingleElement] = useState();
 
-	useEffect(() => {});
+	useEffect(() => {
+		fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${apiKey}`)
+			.then(response => response.json())
+			.then(data => {
+				setSingleElement(data);
+			});
+	}, []);
+
+	console.log(singleElement);
 
 	return (
 		<div>
-			<p>Details</p>
+			<p>{singleElement.title}</p>
 		</div>
 	);
 }
