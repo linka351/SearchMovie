d6cf49f335f1b391fbb135da822c74556636c754
Linka351 <linka351@gmail.com>
2024-02-17 14:43:36 +0100

    correct:

--- search_movie_app/src/components/Slider.js
+++ search_movie_app/src/components/Slider.js
@@ -2,15 +2,17 @@ import { useEffect, useState } from "react";
 import "../styles/slider.scss";
 import { MdOutlineNavigateNext } from "react-icons/md";
 import { MdNavigateBefore } from "react-icons/md";
+import ReactLoading from "react-loading";
 
-const apiKey = "c9de2f7b31706574fa92cef28829a225";
+const apiKey = process.env.REACT_APP_IMAGE_API_KEY;
 const imageCount = 1;
 const IMG_URL = "https://image.tmdb.org/t/p/original/";
 
 function Slider() {
 	const [data, setData] = useState([]);
 	const [activeItemIndex, setActiveItemIndex] = useState(0);
-	const blockNextPageButton = data.length - 1;
+	const isFirstItem = activeItemIndex > 0;
+	const isLastItem = activeItemIndex < data.length - 1;
 
 	const nextTrendingMovie = () => {
 		setActiveItemIndex(prev => prev + 1);
@@ -36,46 +38,44 @@ function Slider() {
 		if (activeItemIndex > lastIndex) {
 			setActiveItemIndex(0);
 		}
-	}, [activeItemIndex, data]);
 
-	useEffect(() => {
 		let slider = setInterval(() => {
 			setActiveItemIndex(activeItemIndex + 1);
 		}, 5000);
 
 		return () => clearInterval(slider);
-	}, [activeItemIndex]);
+	}, [activeItemIndex, data]);
 
 	if (data.length === 0) {
-		return <div>Loading...</div>;
+		return (
+			<ReactLoading type='spin' height={"20%"} color='black' width={"20%"} />
+		);
 	}
 
 	return (
-		<>
-			<div className='slider'>
-				{data.slice(activeItemIndex, activeItemIndex + imageCount).map(item => {
-					return (
-						<>
-							<img alt={item.title} src={`${IMG_URL}${item?.backdrop_path}`} />
-							<div className='description-movie'>
-								<h2>{item.title || item.name}</h2>
-								<p>{item.overview}</p>
-							</div>
-						</>
-					);
-				})}
-				{activeItemIndex > 0 && (
-					<button onClick={prevTrendingMovie} className='btn btn-left'>
-						<MdNavigateBefore />
-					</button>
-				)}
-				{activeItemIndex < blockNextPageButton && (
-					<button onClick={nextTrendingMovie} className='btn btn-right'>
-						<MdOutlineNavigateNext />
-					</button>
-				)}
-			</div>
-		</>
+		<div className='slider'>
+			{data.slice(activeItemIndex, activeItemIndex + imageCount).map(item => {
+				return (
+					<>
+						<img alt={item.title} src={`${IMG_URL}${item?.backdrop_path}`} />
+						<div className='description-movie'>
+							<h2>{item.title || item.name}</h2>
+							<p>{item.overview}</p>
+						</div>
+					</>
+				);
+			})}
+			{isFirstItem && (
+				<button onClick={prevTrendingMovie} className='btn btn-left'>
+					<MdNavigateBefore />
+				</button>
+			)}
+			{isLastItem && (
+				<button onClick={nextTrendingMovie} className='btn btn-right'>
+					<MdOutlineNavigateNext />
+				</button>
+			)}
+		</div>
 	);
 }
 
